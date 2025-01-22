f10617d4ed15fb11e93e1f3620cd65a58fd384d9
ZaydAbdullayev <zeyyadabuzayd@gmail.com>
2025-01-22 22:45:27 +0500

    check 3

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,16 +1,23 @@
 import "./card.css";
 import { Loader } from "../decorations/loader";
-// import AnimatedNumbers from "react-animated-numbers";
+import AnimatedNumbers from "react-animated-numbers";
 
 export const Card = ({ item, prices, ...props }) => {
-  console.log("P", prices);
-
   return (
     <div className="df fdc aic card gap-5" {...props}>
       <img src={item?.icon} alt="icon" />
       <p>{item.name}</p>
       <b className="df aic gap-5">
-        {prices[item.id]?.usd || <Loader small={true} />}$
+        {(
+          <AnimatedNumbers
+            animateToNumber={prices[item.id]?.usd?.toString()}
+            transitions={(index) => ({
+              type: "spring",
+              duration: index + 0.3,
+            })}
+          />
+        ) || <Loader small={true} />}
+        $
       </b>
     </div>
   );
